0ce100d80dd955ea72087fe15aab296cdf8c585f
AMEMELYANOV <emelyanov.am@gmail.com>
2021-02-16 11:30:16 +0300

    3. Интерфейс Input [#463519]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,33 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    private double fuelPrice = 45.50;
+
+    public Bus() {}
+
+    public Bus(double fuelPrice) {this.fuelPrice = fuelPrice;}
+
+    public double getFuelPrice() {
+        return fuelPrice;
+    }
+
+    public void setFuelPrice(double fuelPrice) {
+        this.fuelPrice = fuelPrice;
+    }
+
+    @Override
+    public void go() {
+        System.out.println("Bus is going");
+    }
+
+    @Override
+    public void passengers(int number) {
+        System.out.println("Number of passengers - " + number);
+    }
+
+    @Override
+    public double fill(double fuelQuantity) {
+        return fuelQuantity * this.fuelPrice;
+    }
+}
+
