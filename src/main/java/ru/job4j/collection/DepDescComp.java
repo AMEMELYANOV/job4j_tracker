f22aa19b92317a63d7d9768c6d6a37955dc19a44
AMEMELYANOV <emelyanov.am@gmail.com>
2021-03-07 15:31:01 +0300

    2. Отсортировать департаменты [#463486]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,6 +7,6 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String head1 = o1.split("/")[0];
         String head2 = o2.split("/")[0];
-        return head1.equals(head2) ? o1.compareTo(o2) : o2.compareTo(o1);
+        return head1.equals(head2) ? o1.compareTo(o2) : head2.compareTo(head1);
     }
 }
