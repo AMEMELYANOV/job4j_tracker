cc61cae942da316d5de28bf1d6f0d9edf2103932
AMEMELYANOV <emelyanov.am@gmail.com>
2021-03-08 07:51:05 +0300

    1. Генератор текста

---
+++ src/main/java/ru/job4j/collection/Article.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        boolean rsl = true;
+        List<String> originArr = new ArrayList<>(Arrays.asList(delPuct(origin).split(" ")));
+        List<String> lineArr = new ArrayList<>(Arrays.asList(delPuct(line).split(" ")));
+        for (String s : lineArr) {
+            if (!originArr.contains(s)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+    
+    public static String delPuct(String str) {
+        StringBuilder rsl = new StringBuilder(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isAlphabetic(c) || Character.isDigit(c) || Character.isSpaceChar(c)) {
+                rsl.append(c);
+            }
+        }
+        return rsl.toString();
+    }
+    
+}
