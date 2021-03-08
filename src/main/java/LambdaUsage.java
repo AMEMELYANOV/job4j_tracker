420099d38848726e519912b09c5b51e65ebdc2b4
AMEMELYANOV <emelyanov.am@gmail.com>
2021-03-08 19:06:46 +0300

    2.2. Лямбда блок [#463321]

---
+++ src/main/java/LambdaUsage.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return right.length() - left.length();
+        };
+
+        List<String> lst = Arrays.asList("C", "BB", "CCCC", "AAA");
+        lst.sort(cmpDescSize);
+        for (String s : lst) {
+            System.out.println(s);
+        }
+    }
+}
