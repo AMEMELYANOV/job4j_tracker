73e15923a70e07a0dd263643d54d92e051465daa
AMEMELYANOV <emelyanov.am@gmail.com>
2021-03-12 10:16:37 +0300

    1. Генератор текста

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -8,24 +8,13 @@ import java.util.Set;
 public class Article {
     public static boolean generateBy(String origin, String line) {
         boolean rsl = true;
-        Set<String> set = new HashSet<>(Arrays.asList(delPunctuation(origin).split(" ")));
-        for (String s : delPunctuation(line).split(" ")) {
+        Set<String> set = new HashSet<>(Arrays.asList(origin.replaceAll("\\p{P}", "").split(" ")));
+        for (String s : line.replaceAll("\\p{P}", "").split(" ")) {
             if (!set.contains(s)) {
                 rsl = false;
                 break;
             }
         }
         return rsl;
-    }
-
-    public static String delPunctuation(String str) {
-        StringBuilder rsl = new StringBuilder(str.length());
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (Character.isAlphabetic(c) || Character.isDigit(c) || Character.isSpaceChar(c)) {
-                rsl.append(c);
-            }
-        }
-        return rsl.toString().toLowerCase(Locale.ROOT);
     }
 }
