157f16c3ea66ebb16f8c47a5e1077932cb5b5b18
AMEMELYANOV <emelyanov.am@gmail.com>
2021-03-12 13:36:50 +0300

    2.Проверить две строки на идентичность.

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -4,18 +4,21 @@ import java.util.HashMap;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        return stringToHasMap(left).equals(stringToHasMap(right));
-    }
+        HashMap<String, Integer> leftMap = new HashMap<>();
+        for (String s : left.split("")) {
+            leftMap.computeIfPresent(s, (key, value) -> value + 1);
+            leftMap.putIfAbsent(s, 1);
 
-    public static HashMap<String, Integer> stringToHasMap(String str) {
-        HashMap<String, Integer> result = new HashMap<>();
-        for (String s : str.split("")) {
-            if (!result.containsKey(s)) {
-                result.put(s, 1);
+        }
+        for (String s : right.split("")) {
+            if (!leftMap.containsKey(s)) {
+                return false;
+            } else if (leftMap.get(s) == 1) {
+                leftMap.remove(s);
             } else {
-                result.computeIfPresent(s, (key, value) -> value + 1);
+                leftMap.computeIfPresent(s, (key, value) -> value - 1);
             }
         }
-        return result;
+        return leftMap.isEmpty();
     }
 }
