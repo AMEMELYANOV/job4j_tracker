f7ad2e35bc8bc081ffe7268ff3510bb688dc6c5c
AMEMELYANOV <emelyanov.am@gmail.com>
2021-03-14 18:43:36 +0300

    2. Local-Variable Type Inference [#463341]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -4,24 +4,32 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
     }
 
     public ArrayList<Person> find(String key) {
-        Predicate<Person> phoneContains = person -> person.getPhone().contains(key);
-        Predicate<Person> surnameContains = person -> person.getSurname().contains(key);
-        Predicate<Person> adressContains = person -> person.getAddress().contains(key);
-        Predicate<Person> nameContains = person -> person.getName().contains(key);
-        Predicate<Person> combine = phoneContains
+        Predicate<Person> phoneContains = person -> person
+                .getPhone()
+                .contains(key);
+        Predicate<Person> surnameContains = person -> person
+                .getSurname()
+                .contains(key);
+        Predicate<Person> adressContains = person -> person
+                .getAddress()
+                .contains(key);
+        Predicate<Person> nameContains = person -> person
+                .getName()
+                .contains(key);
+        var combine = phoneContains
                 .or(surnameContains)
                 .or(adressContains)
                 .or(nameContains);
 
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        var result = new ArrayList<Person>();
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
