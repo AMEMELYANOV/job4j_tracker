5a7b86af6d8511efa8981a99cb7d8ef9e6f901ee
AMEMELYANOV <emelyanov.am@gmail.com>
2021-03-22 14:31:55 +0300

    2. Аттестация [#463346]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -25,12 +25,11 @@ public class Analyze {
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream.flatMap(pupil -> pupil.getSubjects().stream())
-                .collect(Collectors.groupingBy(Subject::getName,
+                .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
                         Collectors.averagingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
                 .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
-                .sorted((t1, t2) -> t2.getName().compareTo(t1.getName()))
         .collect(Collectors.toList());
     }
 
